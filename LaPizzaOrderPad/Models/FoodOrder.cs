96f0d4b089e224f895db4f7e207cf184531b11d1
Nicole Bishop <nicolejayne915@hotmail.com>
2018-05-24 14:34:44 -0500

    Starting Commit

---
+++ LaPizzaOrderPad/Models/FoodOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaPizzaOrderPad.Models
+{
+    class FoodOrder
+    {
+        public string FoodItem { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ LaPizzaOrderPad/ViewModels/ShellViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using LaPizzaOrderPad.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LaPizzaOrderPad.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+
+        private BindableCollection<FoodOrder> _order = new BindableCollection<FoodOrder>();
+        private FoodOrder _fullOrder;
+        private string _foodItem;
+        private double _price;
+
+        public ShellViewModel()
+        {
+            Menu.Add(new FoodOrder { FoodItem = "Large", Price = 16.00 });
+            Menu.Add(new FoodOrder { FoodItem = "Medium", Price = 14.50 });
+            Menu.Add(new FoodOrder { FoodItem = "Small", Price =  13.00 });
+
+        }
+    }
+}
